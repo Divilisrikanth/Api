4b718627b8cf5036188187c65c7739494038b52b
Divilisrikanth <srikanthdivili1994@gmail.com>
2024-07-22 10:26:00 +0530

    added route

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const app = express();
 const port = process.env.port || 5000;
 const { products } = require('./data');
 
-<<<<<<< HEAD
 
-app.get('/',(req , res)=>{
-=======
 app.get('/',(req,res)=>{
     res.send('<h2>For Products data click here</h2><a href="/items"</a>products').status(200)
 })
 app.get('/items',(req , res)=>{
->>>>>>> a10d781 (added changes)
     const getItems = products.map((data)=>{
         const{id,name,price,des,category}=data;
          return{id,name,price,des,category}
