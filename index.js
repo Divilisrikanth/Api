a10d7818045019fcd8bf9f6c403e4eeb7e3ff48c
ramakanth47 <diviliramakanth66@gmail.com>
2024-07-22 08:50:44 +0530

    added changes

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = process.env.port || 5000;
 const { products } = require('./data');
 
-
+app.get('/',(req,res)=>{
+    res.send('<h2>For Products data click here</h2><a href="/items"</a>products').status(200)
+})
 app.get('/items',(req , res)=>{
     const getItems = products.map((data)=>{
         const{id,name,price,des,category}=data;
